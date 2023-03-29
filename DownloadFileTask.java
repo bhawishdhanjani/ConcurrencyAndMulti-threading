90f72c86b81882e437da3407b8c8ddb24adfa2ef
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-03-29 17:31:47 +0500

    Code related to Pausing a thread

--- DownloadFileTask.java
+++ DownloadFileTask.java
@@ -6,5 +6,11 @@ public class DownloadFileTask implements Runnable {
     @Override
     public void run() {
         System.out.println("Downloading File" + Thread.currentThread().getName());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Downloading Completed" + Thread.currentThread().getName());
     }
 }
--- PausingThread.java
+++ PausingThread.java
@@ -1,2 +1,14 @@
-package ConcurrencyAndMultiThreading;public class PasssingThread {
+package ConcurrencyAndMultiThreading;
+
+public class PausingThread {
+    public static void show(){
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0 ; i<10 ; i++) {
+            Thread thread = new Thread(new DownloadFileTask());
+            thread.start();
+
+        }
+
+    }
+
 }
