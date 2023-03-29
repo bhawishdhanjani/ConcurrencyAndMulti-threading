4bdd14930d83a497fc83aa0213059d15a0614e3e
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-03-29 17:26:56 +0500

    Code related to starting a thread

--- ThreadDemo.java
+++ ThreadDemo.java
@@ -3,8 +3,11 @@ package ConcurrencyAndMultiThreading;
 public class ThreadDemo {
     public static void show(){
         System.out.println(Thread.currentThread().getName());
-        Thread thread = new Thread(()-> System.out.println("Downloading file "+ Thread.currentThread().getName()));
-        thread.start();
+        for (int i = 0 ; i<10 ; i++) {
+            Thread thread = new Thread(new DownloadFileTask());
+            thread.start();
+        }
+
     }
 
 }
