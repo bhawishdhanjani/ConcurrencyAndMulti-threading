d91f065218ba2a70d4980a383d865c077a35162f
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-03-30 15:30:36 +0500

    Tutorial on Synchronized Keyword

--- DownloadStatus.java
+++ DownloadStatus.java
@@ -1,22 +1,14 @@
 package ConcurrencyAndMultiThreading;
-
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class DownloadStatus {
     private int totalByte;
-    private Lock lock = new ReentrantLock();
+    private Object totalByteLock = new Object();
 
     public int getTotalByteDownload() {
         return totalByte;
     }
     public void incrementTotalByte(){
-        lock.lock();
-        try {
+        synchronized (totalByteLock){
             totalByte++;
-        }
-        finally {
-            lock.unlock();
         }
     }
 }
