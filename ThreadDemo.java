83eb3ec35c6fae03bd2d691ab04ba6afae4bc4bd
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-03-31 15:14:53 +0500

    Tutorial on Concurrent Collection

--- ThreadDemo.java
+++ ThreadDemo.java
@@ -1,15 +1,18 @@
 package ConcurrencyAndMultiThreading;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ThreadDemo {
     public static void show(){
-        Collection<Integer> list = Collections.synchronizedCollection(new ArrayList<>());
-        Thread thread1 = new Thread(()->list.addAll(List.of(1,2,3,4,5,6,7,8,9,10)));
-        Thread thread2 = new Thread(()-> list.addAll(List.of(10,11,12,13,14,15,16,17,18,19,20)));
+        Map<Integer,String> map = new ConcurrentHashMap<>();
+        Thread thread1 = new Thread(()->{
+            map.put(1,"A");
+        });
+        Thread thread2 = new Thread(()->{
+            map.put(2,"B");
+        });
+
         thread1.start();
         thread2.start();
         try {
@@ -18,7 +21,7 @@ public class ThreadDemo {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(list);
+        System.out.println(map);
 
 
     }
